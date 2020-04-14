d2f8a11ec53c789206b09236d9190da74ac6a2cf
afairlie <ariane.fairlie@gmail.com>
2020-04-14 02:02:24 +0000

    add bodyParser library, created urls_new.ejs template

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const server = express();
 const PORT = 8080; // default port 8080
 
 server.set('view engine', 'ejs');
+
+server.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -18,19 +21,20 @@ server.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+server.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+server.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 server.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
 
   res.render('urls_show', templateVars);
-})
-
-/* server.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
 });
-
-server.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-}); */
 
 server.listen(PORT, () => {
   console.log(`Example server listening on port ${PORT}!`);
