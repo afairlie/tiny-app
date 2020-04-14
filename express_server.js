a9528fc2af946046968fe33c617997c86733bf75
afairlie <ariane.fairlie@gmail.com>
2020-04-14 00:06:59 +0000

    added urls_index template, renders urlDatabase object

--- express_server.js
+++ express_server.js
@@ -7,11 +7,16 @@ server.set('view engine', 'ejs');
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
-}
+};
 
 server.get('/', (req, res) => {
   res.send('Hello!');
-})
+});
+
+server.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
 
 server.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -19,8 +24,8 @@ server.get('/urls.json', (req, res) => {
 
 server.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
-})
+});
 
 server.listen(PORT, () => {
-  console.log(`Example server listening on port ${PORT}!`)
-})
+  console.log(`Example server listening on port ${PORT}!`);
+});
