c5548957ac1d765d9ac81fb01aaa83bd79383b45
afairlie <ariane.fairlie@gmail.com>
2020-04-14 23:33:07 +0000

    add login cookie displayed in nav with .post method at /login

--- express_server.js
+++ express_server.js
@@ -1,26 +1,40 @@
+// PACKAGES ETC.
 const express = require('express');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const server = express();
 const PORT = 8080; // default port 8080
 
 server.set('view engine', 'ejs');
 server.use(bodyParser.urlencoded({extended: true}));
+server.use(cookieParser());
 
+// HELPER FUNCTIONS
 function generateRandomString() {
 return Math.random().toString(36).substring(2, 8);
 }
 
+// DB
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+// ROUTING
 server.get('/', (req, res) => {
   res.redirect('/urls');
 });
 
+server.post('/login', (req, res) => {
+  const username = req.body.username;
+  console.log(username);
+  // username = val in header form
+  res.cookie('username', username);
+  res.redirect('/urls');
+})
+
 server.get('/urls', (req, res) => {
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render('urls_index', templateVars);
 });
 
@@ -36,7 +50,7 @@ server.post("/urls", (req, res) => {
 });
 
 server.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
 
   res.render('urls_show', templateVars);
 });
