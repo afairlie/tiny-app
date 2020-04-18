83fa11e4c05941a2a1cca662a77e17fc1ae70377
afairlie <ariane.fairlie@gmail.com>
2020-04-18 00:23:56 +0000

    removed one, left one example in user and url databases

--- data/urlDatabase.js
+++ data/urlDatabase.js
@@ -1,9 +1,7 @@
 
 const urlDatabase = {
-  'b2xVn2': {longURL: 'http://www.lighthouselabs.ca', user_id: 'na0weg' },
-  '9sm5xK': {longURL: 'http://www.google.com', user_id: 'na0weg' },
-  'umc58c': {longURL: 'https://www.instagram.com/shittydinercoffee/', user_id: 'zby6d7' },
-  'tp759j': {longURL: 'http://www.google.com', user_id: 'zby6d7' }
+  // Example URL Object
+  'b2xVn2': {longURL: 'http://www.lighthouselabs.ca', user_id: 'na0weg' }
 };
 
 module.exports = urlDatabase;
--- data/users.js
+++ data/users.js
@@ -1,13 +1,9 @@
 const users = {
+  // Example USER object
   'na0weg': {
     id: 'na0weg',
     email: 'stevethecat@gmail.com',
-    password: 'booboo'
-  },
-  'zby6d7': {
-    id: 'zby6d7',
-    email: 'vassart.max@gmail.com',
-    password: 'livefreeordie'
+    password: 'hashed password'
   }
 };
 
